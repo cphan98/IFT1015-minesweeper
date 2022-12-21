b62cbe4d582a2e009779c8babe42dd6f27b4704e
Cynthia <cynthiaphan98@gmail.com>
2022-12-20 21:20:49 -0500

    Create functions: colonnesHTML, rangeesHTML, tableauHMTL

--- demineur.py
+++ demineur.py
@@ -1,2 +1,29 @@
 # Hoang-Thi-Thi Cynthia Phan 20220019
 # Vincent Hoang 20183549
+
+def colonnesHTML(longueur):
+
+    # La fonction colonnesHTML prend comme paramètre un entier positif et
+    # retourne un texte contenant le code HTML codant le nombre de colonnes à
+    # ajouter dans le tableau.
+
+    pass
+
+
+def rangeesHTML(hauteur):
+
+    # La fonction rangeesHTML prend comme paramètre un entier positif et
+    # retourne un texte contenant le code HTML codant le nombre de rangées à
+    # inclure dans le tableau. Chaque rangée contient aussi le code qui code
+    # les colonnes.
+
+    pass
+
+
+def tableauHTML(longueur, hauteur):
+
+    # La fonction tableauHTML prend comme paramètres deux entiers positifs,
+    # largeur et hauteur, et retourne un texte contenant code HTML qui code
+    # le tableau.
+
+    pass
