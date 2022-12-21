2bfc804dc9e695c5c9128e309edf1091f851a87e
Cynthia <cynthiaphan98@gmail.com>
2022-12-20 22:36:58 -0500

    edit rangeesHTML

--- demineur.py
+++ demineur.py
@@ -1,17 +1,17 @@
 # Hoang-Thi-Thi Cynthia Phan 20220019
 # Vincent Hoang 20183549
 
-def cellulesHTML(longueur):
+def cellulesHTML(largeur):
 
     # La fonction cellulesHTML prend comme paramètre un entier positif et
     # retourne un texte contenant le code HTML codant le nombre de cellules à
     # ajouter dans chaque rangée tableau. Chaque cellule contient une image
     # d'une tuile vide.
 
-    cellules = ''
     if largeur <= 0:
         return -1
     else:
+        cellules = ''
         for i in range(largeur):
             cellules += ('<td id="tuile' + str(i) +
                          '"><img src="http://codeboot.org/images/minesweeper/blank.png"></td>')
@@ -25,7 +25,14 @@ def rangeesHTML(largeur, hauteur):
     # inclure dans le tableau. Chaque rangée contient aussi le code qui code
     # les cellules.
 
-    pass
+    if largeur <= 0 or hauteur <= 0:
+        return -1
+    else:
+        rangees = ''
+        cellules = cellulesHTML(largeur)
+        for _ in range(hauteur):
+            rangees += ('<tr>' + cellules + '</tr>')
+        return rangees
 
 
 def tableauHTML(longueur, hauteur):
@@ -61,3 +68,6 @@ def testDemineur():
     assert rangeesHTML(
         2, 1) == '<tr><td id="tuile0"><img src="http://codeboot.org/images/minesweeper/blank.png"></td><td id="tuile1"><img src="http://codeboot.org/images/minesweeper/blank.png"></td></tr>'
     assert rangeesHTML(1, 2) == '<tr><td id="tuile0"><img src="http://codeboot.org/images/minesweeper/blank.png"></td></tr><tr><td id="tuile0"><img src="http://codeboot.org/images/minesweeper/blank.png"></td></tr>'
+
+
+testDemineur()
